5ab3d1f7cc0fbca8c72eaf57b593692763ae1b2b
pavan901 <pavan@videosdk.live>
2025-06-06 15:50:48 +0530

    quick start

--- android/app/src/main/java/com/quick/MainApplication.kt
+++ android/app/src/main/java/com/quick/MainApplication.kt
@@ -11,16 +11,21 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import live.videosdk.rnwebrtc.WebRTCModulePackage
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+        override fun getPackages(): List<ReactPackage> {
+          // Get the default list of packages
+          val packages = PackageList(this).packages.toMutableList()
+
+          // Add VideoSDK WebRTC package manually
+          packages.add(WebRTCModulePackage())
+
+          return packages
+        }
 
         override fun getJSMainModuleName(): String = "index"
 
--- index.js
+++ index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+// eslint-disable-next-line quotes
+import App from "./App";
+import { name as appName } from './app.json';
+import { register } from '@videosdk.live/react-native-sdk';
+
+register();
 
 AppRegistry.registerComponent(appName, () => App);
